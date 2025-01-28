4e682141985201ef35967b3e3fac919e2e7143a2
Jaswanth-0201 <sidhanijaswanth0201@gmail.com>
2025-01-28 10:13:31 +0530

    Add files via upload

---
+++ 27-01-25 - 2 .py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+data = {
+    "age": [25, 30, 35, 40, 45],
+    "height": [150, 160, 170, 180, 190],
+    "weight": [50, 60, 70, 80, 90],
+}
+
+df = pd.DataFrame(data)
+scaler = MinMaxScaler()
+normalized_data = scaler.fit_transform(df)
+normalized_df = pd.DataFrame(normalized_data, columns=df.columns)
+print(normalized_df)
